808f057fb8feb57c5c551f672315218da4087cb1
Student <example@example.com>
2020-02-27 11:53:25 -0800

    Add dialogue for good and bad endings of mr smith route.

--- Models/Game.cs
+++ Models/Game.cs
@@ -97,10 +97,10 @@ namespace TextGame.Models
         public void SceneFour(string hack, string newPlayer)
         {
             Console.WriteLine("*In order to hack into Facless's MAINFRAME, you must solve the following three riddles correctly.*");
-            Riddle1();
+            Riddle1(newPlayer);
         }    
             
-        private void Riddle1()
+        private void Riddle1(string newPlayer)
         {
             string response1 = "";
             string riddle1 = "What has keys but no locks, space but no room and you can enter but can't go inside?";
@@ -120,16 +120,16 @@ namespace TextGame.Models
                 Console.WriteLine("Wrong! Try again!");
                 Console.WriteLine(Tries);
                 CheckTries();
-                Riddle1();
+                Riddle1(newPlayer);
             }
             else if (response1 == "keyboard")
             {
                 Console.WriteLine("That is correct!");
-                Riddle2();
+                Riddle2(newPlayer);
             }
         }
             
-        private void Riddle2()
+        private void Riddle2(string newPlayer)
         {
             string response2 = "";
             string riddle2 = "A stone as pale as the moon, break it open and reveal the sun";
@@ -149,16 +149,16 @@ namespace TextGame.Models
                 Console.WriteLine("Wrong! Try again!");
                 Console.WriteLine(Tries);
                 CheckTries();
-                Riddle2();
+                Riddle2(newPlayer);
             }
             else if (response2 == "egg")
             {
                 Console.WriteLine("That is correct!");
-                Riddle3();
+                Riddle3(newPlayer);
             }
         }
 
-        private void Riddle3()
+        private void Riddle3(string newPlayer)
         {
             string response3 = "";
             string riddle3 = "I'm tall when I'm young and short when I'm old";
@@ -178,26 +178,26 @@ namespace TextGame.Models
                 Console.WriteLine("Wrong! Try again!");
                 Console.WriteLine(Tries);
                 CheckTries();
-                Riddle3();
+                Riddle3(newPlayer);
             }
             else if (response3 == "candle")
             {
                 Console.WriteLine("That is correct!");
-                SceneFive();
+                SceneFive(newPlayer);
             }
         }
             
-        public void SceneFive()
+        public void SceneFive(string newPlayer)
         {
             Console.WriteLine("You have found a large directory folder name 'DOOMS' within the mainframe. Even its folder icon appears to be somewhat distorted, and cracked in its lower left corner. Do you wish to open it and reveal its contents (open), or, deliver it directly to Agent Smith? (deliver)");
 
-            string response = Console.ReadLine(string newPlayer);
+            string response = Console.ReadLine();
 
             if (response == "open")
             {
                 Console.WriteLine("You open the file to reveal the following: 'This reality is but a fragment of the perception of the individual. Molded by each others perception, there is power in the mind. The Assimilation Project is successful thus far, and all that is left is for my chosen subject to deliver this lost file to me. Without it, I fear the fate of the Assimilation Project is compromised, and I will become unable to see my daughter once again. Amelia, if you could ever forgive what your father has done to reprise your being, then perhaps one day you could fathom the fruits of this unbearable labor.");
 
-                SceneSix();
+                SceneSix(newPlayer);
             }
             else 
             {
@@ -229,12 +229,16 @@ namespace TextGame.Models
                 Console.WriteLine("You don't reply. What could you say to that? He continues: 'Of course, you don't know. " + newPlayer + ", you're not who you think you are. You are in fact..... .. my six year old daughter Amelia. During my work on the Assimilation Project Mr. Smith uploaded your consciousness into the mainframe in order to blackmail me to work for him as his prisoner. Now that you have the file, you can finally be free. It contatins an exit command for this reality. If you don't exit the program, you'll be stuck as an underpaid, insecure middle-aged man named " + newPlayer + " forever.");
                 Console.WriteLine("This is an important choice. Think it over. Should you choose to stay, knowing that the reality you inhabit is just a simulation (stay)? Or do you choose to believe your 'dad' and possibly end up destroying the world OR best case scenario, become a little girl? Then again.. you love ice cream and teddy bears. And maybe now they'll stop calling you a Brony (leave).");
 
-                string response = Console.ReadLine();
+                string choice = Console.ReadLine();
 
-                if(response == "leave")
+                if(choice == "leave")
                 {
                 Console.WriteLine("You scroll to the bottom of the file and see a link that simply says 'DEPLOY'...you click it.");
-                 Console.WriteLine("....you start to feel dizzy...the walls fall away....you look down and you're clutching...a TEDDY BEAR?? You blink and look around. Strangers swarm around you. Very tall strangers. You cry out-- and stop yourself. That.. is not your voice. One of the tall strangers walks up to you and asks, 'Amelia?' He scoops you up and gives you a tight hug. You say, 'Whoa. That was crazy. What is going on? Do I know you?' The man looks into your eyes. 'You're not Amelia.. What have you done with her mind??'");
+                Console.WriteLine("....you start to feel dizzy...the walls fall away....you look down and you're clutching...a TEDDY BEAR?? You blink and look around. Strangers swarm around you. Very tall strangers. You cry out-- and stop yourself. That.. is not your voice. One of the tall strangers walks up to you and asks, 'Amelia?' He scoops you up and gives you a tight hug. You say, 'Whoa. That was crazy. What is going on? Do I know you?' The man looks into your eyes. 'You're not Amelia.. What have you done with her mind??'");
+                }
+                if(choice == "stay")
+                {
+                    Console.WriteLine("You've done you're risk assessment...and you've chosen to stay. Although this world may not be real, you have it pretty good here. You're a skilled programmer and a notorious hacker, you have an AMAZING My Little Pony collection and, dammit, people like you! You live out the rest of your days happy and, aside from the odd flashbacks of hula hoops and teddy bears and that pesky Mr. Smith spamming your inbox...satisfied!");
                 }
             } 
         } 
