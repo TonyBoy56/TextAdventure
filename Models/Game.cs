c2fb23db079c8f8a45dc1d9911107123aba7ce30
Student <example@example.com>
2020-02-26 16:37:12 -0800

    add riddle logic to text adventure

--- Models/Game.cs
+++ Models/Game.cs
@@ -75,48 +75,91 @@ namespace TextGame.Models
             string hack = Console.ReadLine();
             if (hack == "hack")
             {
-                SceneFour();
+                SceneFour(hack, newPlayer);
+            }
+        }
+        public void Riddle(string response, string answer, string riddle, string nextRiddle)
+        {
+            if (response == answer) {
+                Console.WriteLine(nextRiddle);
+            }
+            else
+            {
+                Console.WriteLine(riddle);
             }
         }
         public void SceneFour(string hack, string newPlayer)
         {
-            string response2 = "egg";
-            string response3 = "candle";
-            int tries = 0;
+            string answer1 = "keyboard";
+            string answer2 = "egg";
+            string answer3 = "candle";
+            string success = "Congratulations";
+            // int tries = 0;
             string riddle1 = "What has keys but no locks, space but no room and you can enter but can't go inside?";
             string riddle2 = "A stone as pale as the moom break it open and reveal the sun. What am I?";
             string riddle3 = "I'm tall when I'm young and I'm short when I'm old. What am I?";
             Console.WriteLine("*In order to hack into Facless's MAINFRAME, you must solve the following three riddles correctly.*");
             Console.WriteLine(riddle1);
             string response1 = Console.ReadLine();
-
+            Riddle(response1, answer1, riddle1, riddle2);
+            string response2 = Console.ReadLine();
+            Riddle(response2, answer2, riddle2, riddle3);
+            string response3 = Console.ReadLine();
+            Riddle(response3, answer3, riddle3, success);
+            SceneFive();
             
-            while (tries < 3)
-            {
-                if (response1 != "keyboard")
-                {
-                    Console.WriteLine("Wrong! Try again!");
-                    tries=+ 1;
-                }
-                else
-                {
-                    Console.WriteLine("That is correct!");
-                    Console.WriteLine(riddle2);
-                    response2 = Console.ReadLine();
-                }
+        
+            
+            // while (tries != 3)
+            // {
+            //     while (response1 != "keyboard")
+            //     {
+            //         Console.WriteLine("Wrong! Try again!");
+            //         tries = 1;
+            //         Console.WriteLine(riddle1);
+            //         response1 = Console.ReadLine();
+            //         tries = 2;
+            //         Console.WriteLine(riddle1);
+            //         response1 = Console.ReadLine();
+            //         Console.WriteLine("Wrong! Try again!");
+            //         tries =3;
+            //     }
+            //     if (response1 == "keyboard")
+            //     {
+            //         Console.WriteLine("That is correct!");
+            //         Console.WriteLine(riddle2);
+            //         response2 = Console.ReadLine();
+            //     }
 
-                if (response2 != "egg")
-                {
-                    Console.WriteLine("Wrong! Try again!");
-                    tries += 1;
-                }
-                else 
-                {
-                    Console.WriteLine("That is correct!");
-                    Console.WriteLine(riddle3);
-                    response3 = Console.ReadLine();
-                }
-            }
+            //     while (response2 != "egg")
+            //     {
+            //         Console.WriteLine("Wrong! Try again!");
+            //         tries += 1;
+            //     }
+            //     if (response2 == "egg")
+            //     {
+            //         Console.WriteLine("That is correct!");
+            //         Console.WriteLine(riddle3);
+            //         response3 = Console.ReadLine();
+            //     }
+            //     while (response3 != "candle")
+            //     {
+            //         Console.WriteLine("Wrong! Try again!");
+            //         Console.WriteLine("GAME OVER. You have failed in your journey");
+            //         tries += 1;
+            //     }
+            //     if (response3 == "candle")
+            //     {
+            //         Console.WriteLine("You have successfully hacked the mainframe!");
+            //         tries = 3;
+            //         SceneFive();
+            //     }
+            // }
+        }
+
+        public void SceneFive()
+        {
+            Console.WriteLine("You have found a large directory folder name 'DOOMS' within the mainframe. Even its folder icon appears to be somewhat distorted, and cracked in its lower left corner. Do you wish to open it, and reveal its contents?");
         }
     }
 }
